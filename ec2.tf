cbccd9bed0ccdcc56d0b28c9f206e8a2bf0d95a2
CodingManoj <mvp2612@hotmail.com>
2023-02-27 20:11:48 -0500

    Auto Push Commit

--- ec2.tf
+++ ec2.tf
@@ -37,5 +37,5 @@ resource "aws_ec2_tag" "spot-server-tag" {
 }
 
 locals {
-    ALL_INSTANCE_IDS = concat(aws_spot_instance_request.spot-server.*.)
+    ALL_INSTANCE_IDS = concat(aws_spot_instance_request.spot-server.*.spot_instance_id, aws_instance.my-ec2.*.id)
 }
