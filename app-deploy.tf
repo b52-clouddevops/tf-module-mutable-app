003136f1690622f1c1642db2393f5dee392221c9
CodingManoj <mvp2612@hotmail.com>
2023-02-27 20:52:28 -0500

    Auto Push Commit

--- app-deploy.tf
+++ app-deploy.tf
@@ -2,7 +2,7 @@ resource "null_resource" "app" {
 
   triggers = {
     # version = var.APP_VERSION   #only when there is a change in the value of APP_VERSION compated to previous verion, then only this will be triggered.
-      timechange = 
+      timechange = timestamp()
   }
 
   count   = var.OD_INSTANCE_COUNT + var.SPOT_INSTANCE_COUNT
