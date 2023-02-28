becaa243cfb8bdf47050b822754899b1d5c4555e
CodingManoj <mvp2612@hotmail.com>
2023-02-27 19:52:18 -0500

    Auto Push Commit

--- ec2.tf
+++ ec2.tf
@@ -5,6 +5,7 @@ resource "aws_instance" "my-ec2" {
   count                = var.OD_INSTANCE_COUNT
   ami                     = data.aws_ami.lab-image.id
   instance_type           = var.INSTANCE_TYPE
+  subnet_id               = data.
   vpc_security_group_ids  = [aws_security_group.allow_app.id]
 
   tags = {
