b58ad8d59235ddaae263ba7b854a9bfcfd011996
CodingManoj <mvp2612@hotmail.com>
2023-02-27 20:19:56 -0500

    Auto Push Commit

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
     ALL_INSTANCE_IDS = concat(aws_spot_instance_request.spot-server.*.spot_instance_id, aws_instance.my-ec2.*.id)
     ALL_INSTANCE_IPS = concat(aws_spot_instance_request.spot-server.*.private_ip, aws_instance.my-ec2.*.private_ip)
-    SSH_USER         = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["SSH_USERNAME"]
-    SSH_PASSWORD     = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["DOCDB_PASSWORD"]
+    SSH_USER         = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["SSH_USER"]
+    SSH_PASSWORD     = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["SSH_PASSWORD"]
 }
