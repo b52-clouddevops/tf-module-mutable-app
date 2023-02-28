5090d0b4bfcefce996a5709512d33e5713928e9b
CodingManoj <mvp2612@hotmail.com>
2023-02-27 20:45:00 -0500

    Auto Push Commit

--- app-deploy.tf
+++ app-deploy.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "app" {
 
-  count   = var.OD_INSTANCE_COUNT + SPOT_INSTANCE_COUNT
+  count   = var.OD_INSTANCE_COUNT + var.SPOT_INSTANCE_COUNT
 
   provisioner "remote-exec" {
     connection {
