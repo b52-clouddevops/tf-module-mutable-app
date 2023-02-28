1794c29c95cd93f8762cc27cba1db1d42ba99bca
CodingManoj <mvp2612@hotmail.com>
2023-02-27 19:55:07 -0500

    Auto Push Commit

--- ec2.tf
+++ ec2.tf
@@ -16,9 +16,9 @@ resource "aws_instance" "my-ec2" {
 
 # Creates SPOT Servers
 resource "aws_spot_instance_request" "cheap_worker" {
-  count                  = var.SPOT_INSTANCE_COUNT
-  ami                    = data.aws_ami.lab-image.id
-  instance_type          = var.INSTANCE_TYPE
+  count                   = var.SPOT_INSTANCE_COUNT
+  ami                     = data.aws_ami.lab-image.id
+  instance_type           = var.INSTANCE_TYPE
   subnet_id               = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS, count.index)
   vpc_security_group_ids  = [aws_security_group.allow_app.id]
   wait_for_fulfillment = true
