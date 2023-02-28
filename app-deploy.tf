85eb18e2c87e8ed42b81cfc2978952f747c9712d
CodingManoj <mvp2612@hotmail.com>
2023-02-27 20:16:30 -0500

    Auto Push Commit

--- app-deploy.tf
+++ app-deploy.tf
@@ -1,11 +1,11 @@
 resource "null_resource" "app" {
-    
+
   provisioner "remote-exec" {
     connection {
       type     = "ssh"
       user     = "centos"
       password = "DevOps321"
-      host     = self.public_ip         # self. will only work if it's inside the resource ; If not, we need to use aws_instance.my-ec2.public_ip
+      host     =       
       }
 
       inline = [
