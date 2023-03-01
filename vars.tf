ee4000d43de7e00010ee65fb60f467a29b4ddb8e
CodingManoj <mvp2612@hotmail.com>
2023-03-01 18:24:43 -0500

    AutoPush

--- vars.tf
+++ vars.tf
@@ -3,8 +3,9 @@ variable "INSTANCE_TYPE" {}
 variable "SPOT_INSTANCE_COUNT" {}
 variable "OD_INSTANCE_COUNT" {}
 variable "COMPONENT" {}
-variable "APP_PORT" {}
 variable "APP_VERSION" {}
+variable "APP_PORT" {}
+
 
 
 
