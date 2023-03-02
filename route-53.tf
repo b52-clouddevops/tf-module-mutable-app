652761c857e158f4c5317052a9b2fde0f9ab97b1
CodingManoj <mvp2612@hotmail.com>
2023-03-01 20:17:37 -0500

    Auto Push Commit

--- route-53.tf
+++ route-53.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "dns-record" {
-  zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTEDZONE_ID
+  zone_id = var.ALB_TYPE == "internal" ? data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTEDZONE_ID :
   name    = "${var.COMPONENT}-dev.roboshop.internal"
   type    = "CNAME"
   ttl     = 10
-  records = var.ALB_TYPE == "internal" ? [data.terraform_remote_state.alb.outputs.PRIVATE_ALB_ADDRESS] : [data.terraform_remote_state.alb.outputs.PRIVATE_ALB_ADDRESS]
+  records = var.ALB_TYPE == "internal" ? [data.terraform_remote_state.alb.outputs.PRIVATE_ALB_ADDRESS] : [data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ADDRESS]
 }
