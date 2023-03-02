94ace6a9aa90aa237da563114ff8e02233fdcc06
CodingManoj <mvp2612@hotmail.com>
2023-03-01 19:40:24 -0500

    Auto Push Commit

--- target-group.tf
+++ target-group.tf
@@ -1,8 +1,7 @@
 #  Creates the target group.
-
 resource "aws_lb_target_group" "app-tg" {
   name     = "${var.COMPONENT}-${var.ENV}"
   port     = 8080
   protocol = "HTTP"
-  vpc_id   = data.terraform_remote_state.vpc.outputs
+  vpc_id   = data.terraform_remote_state.vpc.outputs.VPC_ID
 }
