04c9856f24ded7aa9696ba58bd9d10e51932059f
CodingManoj <mvp2612@hotmail.com>
2023-03-01 20:13:13 -0500

    Auto Push Commit

--- route-53.tf
+++ route-53.tf
@@ -1,4 +1,4 @@
-resource "aws_route53_record" "dns-rec" {
+resource "aws_route53_record" "dns-record" {
   zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTEDZONE_ID
   name    = "${var.COMPONENT}-dev.roboshop.internal"
   type    = "CNAME"
