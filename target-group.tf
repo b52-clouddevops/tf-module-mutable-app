0cd22854ba2d6889cf9e7b637f97aee37ffde263
CodingManoj <mvp2612@hotmail.com>
2023-03-02 20:04:17 -0500

    Auto Push Commit

--- target-group.tf
+++ target-group.tf
@@ -27,7 +27,7 @@ resource "random_integer" "priority" {
 
 # Adding rules to the created public target group
 resource "aws_lb_listener_rule" "private-tg-rule" {
-  count        = var.ALB_TYPE == "internal" ? 1 : 1
+  count        = var.ALB_TYPE == "internal" ? 1 : 0
 
   listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
   priority     = random_integer.priority.result
