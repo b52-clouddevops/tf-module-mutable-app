a0d27eacdd1e81baae2fd5f3138bd16d1ce81aea
CodingManoj <mvp2612@hotmail.com>
2023-03-02 19:46:02 -0500

    Auto Push Commit

--- target-group.tf
+++ target-group.tf
@@ -40,3 +40,18 @@ resource "random_integer" "priority" {
   min = 100
   max = 800
 }
+
+
+# Creating a listener in the public load-balancer
+resource "aws_lb_listener" "public" {
+  count             = var.INTERNAL ? 0 : 1
+
+  load_balancer_arn = data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ARN
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app-tg.arn
+  }
+}
